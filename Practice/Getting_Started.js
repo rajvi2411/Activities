9107b38d408015ac23fb89a98e38f6ff0c02f13e
Rajviba Gohil <rajvibagohil2411@FVFGV603Q05N.local>
2022-02-25 23:52:42 +0530

    24-FRB-2022

--- Practice/Getting_Started.js
+++ Practice/Getting_Started.js
@@ -21,5 +21,45 @@
 // console.log(age); // Prints the value inserted by the user
 // if(window.confirm("Are you sure you want to delete this?")) { deleteItem(itemId);
 // }
-var img = new Image();
-img.src = 'https://i.ytimg.com/vi/zecueq-mo4M/maxresdefault.jpg'; document.body.appendChild(img);
+// var img = new Image();
+// img.src = 'https://i.ytimg.com/vi/zecueq-mo4M/maxresdefault.jpg'; document.body.appendChild(img);
+
+///Variables in JS
+
+// var myVariable = "This is a variable!";
+// console.log(myVariable);
+
+// var myString = "I am a " + "string!";
+// console.log(myString);
+// var notDefined; // undefined: we didn't define it to anything yet 
+// window.alert("aRandomVariable"); 
+
+// var favoriteFruits = ["apple", "orange", "strawberry"];
+// var carsInParkingLot = ["Toyota", "Ferrari", "Lexus"];
+// var employees = ["Billy", "Bob", "Joe"];
+// var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
+// var randomVariables = [2, "any type works", undefined, null, true, 2.51];
+// myArray = ["zero", "one", "two"];
+// window.alert(myArray[0]); // 0 is the first element of an array
+//                            // in this case, the value would be "zero"
+// myArray = ["John Doe", "Billy"];
+// elementNumber = 1;
+// window.alert(myArray[elementNumber]); // Billy
+
+// myObject = {};
+// Person1 = {
+//     firstname: "John", 
+//     lastname: "Doe", 
+//     fullname: "John Doe"
+// };
+// billy = {
+// firstname: "Billy", 
+// lastname: undefined, 
+// fullname: "Billy"
+// };
+// window.alert(Person1.fullname); // John Doe 
+// window.alert(billy.firstname); // Billy
+
+console.log(typeof null);
+console.log(typeof undefined); // == is true as they ae same in value but === is false as they are not equal in datatypes
+
